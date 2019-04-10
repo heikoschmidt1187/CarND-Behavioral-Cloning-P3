63feed7381dc4c7bb356d8eaad32e879f2c9ff2d
heikoschmidt1187 <github@heikoschmidt.info>
2019-04-10 19:36:15 +0200

    Implemented LeNet

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
 
 # read the csv file and store the data
 lines = []
@@ -38,16 +38,32 @@ for line in lines:
 X_train = np.array(images)
 y_train = np.array(measurements)
 
+# Own LeNet for test
+
 # create a model
 model = Sequential()
 
 # preprocess image befor processing in the network
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 
-# add flatten layer
+# First convolutional layer
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+
+# First Max Pooling Layer
+model.add(MaxPooling2D())
+
+# Second convolutional layer
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+
+# Second Max Pooling Layer
+model.add(MaxPooling2D())
+
+# Flatten layer
 model.add(Flatten())
 
-# add dense output layer
+# fully connected layers
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 # compile model, use mean square error loss function as it's a continuous output with regression
